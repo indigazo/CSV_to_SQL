4732aac37e9356f4efada0bcd6ea749621007cbc
Eduardo Ortega <eduardo.ortega.ing94@outlook.es>
2019-12-20 16:40:03 -0300

    Cambio de nombre de variable para mejor entendimiento

--- csv_to_sql.py
+++ csv_to_sql.py
@@ -6,9 +6,9 @@ import argparse
 ### MAIN PROGRAM ###
 
 def run(args):
-	t_name = args.t_name
-	f_name = args.f_name
-	out_name = args.out_name
+	t_name = args.table_name
+	f_name = args.csv_name
+	out_name = args.sql_name
 	if writeInsert(f_name, t_name, out_name):
 		print(f"Archivo SQL creado con éxito en {out_name}")
 		sleep(1)
@@ -16,9 +16,9 @@ def run(args):
 
 def main():
 	parser=argparse.ArgumentParser(description="Retorna una sentencia SQL enriquecida por un archivo CSV")
-	parser.add_argument("-tb",help="Nombre de la Tabla en la que se insertarán los datos" ,dest="t_name", type=str, required=True)
-	parser.add_argument("-in",help="Directorio y nombre del archivo .CSV" ,dest="f_name", type=str, required=True)	
-	parser.add_argument("-out",help="Directorio y nombre del archivo .SQL" ,dest="out_name", type=str, required=True)	
+	parser.add_argument("-tb",help="Nombre de la Tabla en la que se insertarán los datos" ,dest="table_name", type=str, required=True)
+	parser.add_argument("-in",help="Directorio y nombre del archivo .CSV" ,dest="csv_name", type=str, required=True)	
+	parser.add_argument("-out",help="Directorio y nombre del archivo .SQL" ,dest="sql_name", type=str, required=True)	
 	parser.set_defaults(func=run)
 	args=parser.parse_args()
 	args.func(args)
