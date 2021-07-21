b4dd8c987b47e8fd010fb2828edf2a7ab14d1eee
Edu <indigomaker@gmail.com>
2021-07-21 08:47:27 -0400

    Comienzo de front end, trabajando con argparse, juntar con backend y empaquetar

--- csv_to_sql.py
+++ csv_to_sql.py
@@ -1,9 +1,6 @@
-import csv
-import os
 import argparse
-
-from back_end import *
 import time
+from back_end import *
 from pprint import pprint
 
 # front end useful functions
@@ -16,26 +13,29 @@ def get_class_from_dict(file, o_file, table_name, key=SQL_FORMAT.SQL_SERVER):
 
 ### MAIN PROGRAM ###
 
-# def run(args):
-# 	t_name = args.table_name
-# 	f_name = args.csv_name
-# 	out_name = args.sql_name
-# 	if write_insert(f_name, t_name, out_name):
-# 		print(f"Archivo SQL creado con éxito en {out_name}")
-# 		sleep(1)
-# 		os.system(out_name) # Corre el archivo creado 
+def main_function_loop(args):
+	print(args.sql_type)
+	pass
+	# t_name = args.table_name
+	# f_name = args.csv_name
+	# out_name = args.sql_name
+	# if write_insert(f_name, t_name, out_name):
+	# 	print(f"Archivo SQL creado con éxito en {out_name}")
+	# 	sleep(1)
+	# 	os.system(out_name) # Corre el archivo creado 
 
-# def main():
-# 	parser=argparse.ArgumentParser(description="Retorna una sentencia SQL enriquecida por un archivo CSV")
-# 	parser.add_argument("-tb",help="Nombre de la Tabla en la que se insertarán los datos" ,dest="table_name", type=str, required=True)
-# 	parser.add_argument("-in",help="Directorio y nombre del archivo .CSV" ,dest="csv_name", type=str, required=True)	
-# 	parser.add_argument("-out",help="Directorio y nombre del archivo .SQL" ,dest="sql_name", type=str, required=True)	
-# 	parser.set_defaults(func=run)
-# 	args=parser.parse_args()
-# 	args.func(args)
+def arg_parser_entry():
+	parser = argparse.ArgumentParser(description = "Retorna una sentencia SQL enriquecida por un archivo CSV")
+	parser.add_argument("-i",help="ruta archivo .CSV" ,dest="csv_name", type=str, required=True)	
+	parser.add_argument("-o",help="ruta salida archivo .SQL" ,dest="sql_name", type=str, required=True)	
+	parser.add_argument("-t",help="Nombre de la Tabla en la que se insertarán los datos" ,dest="table_name", type=str, required=True)
+	parser.add_argument("-s",help="Tipo de SQL en el que crear archivo. Si se deja en blanco es SQL server" ,dest="sql_type", type=str, required=False)
+	parser.set_defaults(func = main_function_loop)
+	args=parser.parse_args()
+	args.func(args)
 
 # This is only proof of concept main, the final product uses argparse
-def main():
+def test_main():
 
     # class setup
 	Q = get_class_from_dict(
@@ -53,7 +53,9 @@ def main():
 
 if __name__=="__main__":
 	start_time = time.time()
-	
-	main()
-	
+
+	# test_main()
+
+	arg_parser_entry()
+
 	print("** Programa terminado en %s segundos **" % (time.time() - start_time))
